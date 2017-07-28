b3694067abdb1f0f6668bb3ea7398436ca1259ca
MrGramps91 <texas9137@gmail.com>
2017-07-28 10:36:44 -0400

    Update Sudoku.py
    Corrected and reformatted script

--- Sudoku.py
+++ Sudoku.py
@@ -1,195 +1,200 @@
 import argparse
-from Tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
+from tkinter import Tk, Canvas, Frame, Button, BOTH, TOP, BOTTOM
+from pip._vendor.requests.packages.urllib3.connectionpool import xrange
+# Global variables that sets the board###
 
-###Global variables that sets the board###
-BOARDS = ['debug', 'n00b','l33t', 'error'] #Available sudoku boards
-MARGIN = 20 #pixles around the board
-SIDE = 50 #Width of every board cell
-WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9 # Width and height of the whole board
 
-#Application specific errors
+BOARDS = ['debug', 'n00b','l33t', 'error']  # Available sudoku boards
+MARGIN = 20  # pixels around the board
+SIDE = 50  # Width of every board cell
+WIDTH = HEIGHT = MARGIN * 2 + SIDE * 9  # Width and height of the whole board
+
+# Application specific errors
 class SudokuError(Exception):
     pass
 
-#Board representation
+    # board representation
 class SudokuBoard(object):
     def __init__(self, board_file):
-        self.board = __create_board(board_file)
+        self.board = board_file
+        self.board = self.__create_board(board_file)
 
-       #iterate over each line an character, if it does not equal 9 raise error else
-       #return  constructed board
-    def __create_board(self,board_file):
+        # iterate over each line an character, if it does not equal 9 raise error else
+        # return  constructed board
+        def __create_board(self,board_file):
 
-         #creates the list/matrix to store the line and characters
-         board = []
+            # creates the list/matrix to store the line and characters
+            board = []
 
-         #iterate over each line
-         for line in board_file:
-             line = line.strip()
+            # iterate over each line
+            for line in board_file:
+                line = line.strip()
 
-             if len(line) != 9:
-                 board = []
-                 raise SudokuError("Each line in the sudoku puzzle must be 9 chars ling.")
+                if len(line) != 9:
+                    board = []
+                    raise SudokuError("Each line in the sudoku puzzle must be 9 chars ling.")
 
-              #create a list for the line
-                 board.append([])
+                    # create a list for the line
+                    board.append([])
 
-              #iterate over each character
-                 for c in line:
-                  if not c.isdigit():
-                      raise SudokuError("Valid chars for the sudoku puzzle must be in 0-9")
-                  board[-1].append(int(c))
-         if len (board) !=9:
-             raise SudokuError("Each sudoku puzzle must be 9 lines long")
+                    # iterate over each character
+                    for c in line:
+                        if not c.isdigit():
+                            raise SudokuError("Valid chars for the sudoku puzzle must be in 0-9")
+                    board[-1].append(int(c))
+            if len (board) !=9:
+                raise SudokuError("Each sudoku puzzle must be 9 lines long")
 
-        #return constructed board
-         return board
-         pass
+                # return constructed board
+            return board
+        pass
 
-
-#defining and maintaining the state of the game checking whether or no the puzzle is completd
+        # defining and maintaining the state of the game checking whether or no the puzzle is completd
 class SudokuGame(object):
 
     def __init__(self,board_file):
         self.board_file = board_file
-        self.start_puzzle = Sudokuboard(board_file).board
+        self.start_puzzle = self.Sudokuboard(board_file).board
 
-    def start(self):
-        self.game_over = False
-        self.puzzle = []
-        for i in xrange(9):
-            self.puzzle.append([])
-            for j in xrange(9):
-                self.puzzle[i].append(self.start_puzzle[i][j])
+        def start(self):
+            self.game_over = False
+            self.puzzle = []
+            for i in xrange(9):
+                self.puzzle.append([])
+                for j in xrange(9):
+                    self.puzzle[i].append(self.start_puzzle[i][j])
 
     def check_win(self):
-        for row in xrange(9):
-            if not self.__check_row(row):
-                return False
-        for column in xrange(9):
-            if not self.__check_column(column):
-                return False
-        for row in xrange(3):
-            for column in xrange(3):
-                if not self.__check_square(row,column):
+            for row in xrange(9):
+                if not self.__check_row(row):
                     return False
-        self.game_over = True
-        return True
+            for column in xrange(9):
+                if not self.__check_column(column):
+                    return False
+            for row in xrange(3):
+                for column in xrange(3):
+                    if not self.__check_square(row,column):
+                        return False
+            self.game_over = True
+            return True
 
-    #check each row/column to ensure all inputs are ranged from 1 to 10
+        # check each row/column to ensure all inputs are ranged from 1 to 10
     def __check_block(self,block):
-        return set(block) == set(range(1,10))
+            return set(block) == set(range(1,10))
     def __check_row(self,row):
         return self.__check_block(self.puzzle[row])
     def __check_column(self,column):
-        return self.__check_block(
-            [self.puzzle[row][column]for row in xrange(9)]
+            return self.__check_block(
+                [self.puzzle[row][column]for row in xrange(9)]
             )
     def __check_square(self,row,column):
-        return self.__check_block(
-            [
-            self.puzzle[r][c]
-            for r in xrange(row*3,(row+1)*3)
-            for c in xrange(column*3,(column+1)*3)
-            ]
-    )
+            return self.__check_block(
+                [
+                    self.puzzle[r][c]
+                    for r in xrange(row*3,(row+1)*3)
+                    for c in xrange(column*3,(column+1)*3)
+                ]
+            )
 
 
-#creates board and accepts user input
+            # creates board and accepts user input
+
 class SudokuUI(Frame):
-    def __init__(self,parent,game):
+    def __init__(self, parent, game):
         self.game = game
         self.parent = parent
-        Frame.__init__(self,parent)
+        Frame.__init__(self, parent)
 
         self.row, self.col = 0,0
         self.__initUI()
 
     def __initUI(self):
-        self.parent.title("Sudoku")
-        self.pack(fill=BOTH, epand=1)
-        self.canvas = Canvas(self,
-                             width=WIDTH
-                             height = HEIGHT)
-        self.canvas.pack(fill=BOTH, side=TOP)
-        clear_button = Button(self,
-                              text="Clear answers",
-                              command=self.__clear_answers)
-        clear_button.pack(fill=BOTH, side=BOTTOM)
-        self.__draw_grid()
-        self.__draw_puzzle()
+            self.parent.title("Sudoku")
+            self.pack(fill=BOTH, expand=1)
+            self.canvas = Canvas(self,
+                                 width = WIDTH,
+                                 height = HEIGHT)
+            self.canvas.pack(fill=BOTH, side=TOP)
+            clear_button = Button(self,
+                                  text="Clear answers",
+                                  command=self.__clear_answers)
+            clear_button.pack(fill=BOTH, side=BOTTOM)
+            self.__draw_grid()
+            self.__draw_puzzle()
 
-        self.canvas.bind("<Button-1>",self.__cell_clicked)
-        self.canvas.bind("<Key>",self.__key_pressed)
+            self.canvas.bind("<Button-1>",self.__cell_clicked)
+            self.canvas.bind("<Key>",self.__key_pressed)
 
-    #Draws grid with blue lines into 3X3 Squares
+        # Draws grid with blue lines into 3X3 Squares
     def __draw_grid(self):
-        for i in xrange(10):
-            color = "blue" if i % 3 == 0 else "gray"
+            for i in xrange(10):
+                color = "blue" if i % 3 == 0 else "gray"
 
-            x0 = MARGIN + i * SIDE
-            y0 = MARGIN
-            x1 = MARGIN 9 i * SIDE
-            y1 = HEIGHT - MARGIN
-            self.canvas.create_line(x0,y0,x1,y1,fill=color)
+                x0 = MARGIN + i * SIDE
+                y0 = MARGIN
+                x1 = MARGIN + i * SIDE
+                y1 = HEIGHT - MARGIN
+                self.canvas.create_line(x0,y0,x1,y1,fill=color)
 
-            x0 = MARGIN
-            y0 = MARGIN + i * SIDE
-            x1 = WIDTH - MARGIN
-            y1 = MARGIN + i * SIDE
-            self.canvas.create_line(x0,y0,x1,y1,fill=color)
+                x0 = MARGIN
+                y0 = MARGIN + i * SIDE
+                x1 = WIDTH - MARGIN
+                y1 = MARGIN + i * SIDE
+                self.canvas.create_line(x0,y0,x1,y1,fill=color)
 
     def __draw_puzzle(self):
-        self.canvas.delete("number")
-        for i in xrange(9):
-            for j in xrange(9):
-                answer = self.game.puzzle[i][j]
-                if answer != 0:
-                    x = MARGIN + j * SIDE + SIDE / 2
-                    y = MARGIN + i * SIDE + SIDE / 2
-                    orginal = self.game.start_puzzle[i][j]
-                    color = "black" if answer == orignal else "sea green"
-                    self.canvas.create_text(
-                        x,y, text=answer, tags"numbers", fill=color
+            self.canvas.delete("numbers")
+            for i in xrange(9):
+                for j in xrange(9):
+                    answer = self.game.puzzle[i][j]
+                    if answer != 0:
+                        x = MARGIN + j * SIDE + SIDE / 2
+                        y = MARGIN + i * SIDE + SIDE / 2
+                        original = self.game.start_puzzle[i][j]
+                        color = "black" if answer == original else "sea green"
+                        self.canvas.create_text(
+                            x,y, text=answer, tags="numbers", fill=color
                         )
-      def __clear_answers(self):
-          self.game.start()
-          self.canvas.delete("victory")
-          self.__draw_puzzle()
+
+    def __clear_answers(self):
+            self.game.start()
+            self.canvas.delete("victory")
+            self.__draw_puzzle()
 
     def __cell_clicked(self,event):
-        if self.game.game_over:
-            return
-        x,y = event.x, event.y
-        if (MARGIN < x < WIDTH - MARGIN and MARGIN < y < HEIGHT - MARGIN):
-            self.canvas.focus_set()
+            if self.game.game_over:
+                return
+            x,y = event.x, event.y
+            if (MARGIN < x < WIDTH - MARGIN and MARGIN < y < HEIGHT - MARGIN):
+                self.canvas.focus_set()
 
-            #get row and col numbers from x,y coordinates
-            row, col = (y = MARGIN) / SIDE, (x - MARGIN) / SIDE
+                # get row and col numbers from x,y coordinates
+                row, col = (y - MARGIN) / SIDE, (x - MARGIN) / SIDE
 
-            #if cell was selected already - desleect it
-            if (row, col) == (self.row, self.col):
-                self.row, self.col = -1, -1
+                # if cell was selected already - desleect it
+                if (row, col) == (self.row, self.col):
+                    self.row, self.col = -1, -1
                 elif self.game.puzzle[row][col] == 0:
                     self.row, self.col = row, col
-         self.__draw_cursor()
-         pass
+
+            self.__draw_cursor()
 
 
-     def __draw_cursor(self):
-         self.canvas.delete("cursor")
-         if self.row >= 0 and self.col >= 0:
-             x0 = MARGIN + self.col * SIDE + 1
-             y0 = MARGIN + self.row * SIDE + 1
-             x1 = MARGIN + (self.col + 1) * SIDE - 1
-             y1 = MARGIN + (self.row + 1) * SIDE - 1
-             self.canvas.create_rectangle(
-                 x0, y0, x1, y1,
-                 outline = "red", tags="cursor"
-             )
-      def __key_pressed(self, event):
-          if self.game.game_over:
-              return
+    def __draw_cursor(self):
+            self.canvas.delete("cursor")
+            if self.row >= 0 and self.col >= 0:
+                x0 = MARGIN + self.col * SIDE + 1
+                y0 = MARGIN + self.row * SIDE + 1
+                x1 = MARGIN + (self.col + 1) * SIDE - 1
+                y1 = MARGIN + (self.row + 1) * SIDE - 1
+                self.canvas.create_rectangle(
+                    x0, y0, x1, y1,
+                    outline = "red", tags="cursor"
+                )
+
+    def __key_pressed(self, event):
+            if self.game.game_over:
+                return
             if self.row >= 0 and self.col >= 0 and event.char in "1234567890":
                 self.game.puzzle[self.row][self.col] = int(event.char)
                 self.col, self.row = -1, -1
@@ -198,45 +203,45 @@ class SudokuUI(Frame):
                 if self.game.check_win():
                     self.__draw_victory()
 
-        def __draw_victory(self):
-            #creat a oval (which will be a circle)
-            x0 = y0 = MARGIN + SIDE * 2
-            x1 = y1 = MARGIN + SIDE * 7
-            self.canvas.create_oval(
-                x0, y0, x1,y1,
-                tage="Victory",fill="dark orange", outline="orange"
-                )
-            #create text
-            x = y = MARGIN + 4 * SIDE + SIDE / 2
-            self.canvas.create_text(
-                x, y,
-                text="You Win!", tags="winner",
-                fill="white", font=("Arial", 32)
-                )
-
-         # Parses arguments of the form: sudoku.py <board name> Where 'board name'
-         # must be in the 'BOARD' list
-         def parse_arguments():
-             arg_parser = argparse.ArgumentParser()
-             arg_parser.add_argument("--board",
-                                     help="Desired board name",
-                                     type=str,
-                                     choices=BOARDS,
-                                     required=True)
-            # Creats a dictionary of keys = argument flag, and value = argument
-             args = arg_parser.parse_args()
-             return args['board']
+    def __draw_victory(self):
+            # create a oval (which will be a circle)
+        x0 = y0 = MARGIN + SIDE * 2
+        x1 = y1 = MARGIN + SIDE * 7
+        self.canvas.create_oval(
+            x0, y0, x1,y1,
+            tage="Victory",fill="dark orange", outline="orange"
+        )
+        # create text
+        x = y = MARGIN + 4 * SIDE + SIDE / 2
+        self.canvas.create_text(
+            x, y,
+            text="You Win!", tags="winner",
+            fill="white", font=("Arial", 32)
+        )
 
 
-        if __name__=='__main__':
-            board_name = parse_arguments()
+def parse_arguments(self):
+    # Parses arguments of the form: sudoku.py <board name> Where 'board name'
+    # must be in the 'BOARD' list
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--board",
+                            help="Desired board name",
+                            type=str,
+                            choices=BOARDS,
+                            required=True)
+    # Creates a dictionary of keys = argument flag, and value = argument
+    args = arg_parser.parse_args()
+    return args['board']
 
-            with open('%s.sudoku' % board_name,'r') as boards_file:
-                game = SudokuGame(boards_file)
-                game.start()
 
-                root = Tk()
-                SudokuUI(root, game)
-                root.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
-                root.mainloop()
+if __name__=='__main__':
+    board_name = parse_arguments()
 
+    with open('%s.sudoku' % board_name,'r') as boards_file:
+        game = SudokuGame(boards_file)
+        game.start()
+
+        root = Tk()
+        SudokuUI(root, game)
+        root.geometry("%dx%d" % (WIDTH, HEIGHT + 40))
+        root.mainloop()
